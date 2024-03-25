e970a7bc2cc9844834fdc7b4c85def5a08b8a5ad
PedroBahry <budokkanpvp10@Gmail.com>
2024-03-25 10:39:25 -0300

    EX

---
+++ Exercicio7.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Exercicio7 {
+    public static void executar() {
+
+        Scanner scanner = new Scanner(System.in);
+        double[] nota = new double[5];
+        double[] peso = new double[5];
+        double somaNotas = 0.0;
+        double somaPesos = 0.0;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Digite a " + (i + 1) + "ª nota:");
+            nota[i] = scanner.nextDouble();
+
+            System.out.println("Digite o peso da " + (i + 1) + "ª nota:");
+            peso[i] = scanner.nextDouble();
+
+            somaNotas += nota[i] * peso[i];
+            somaPesos += peso[i];
+        }
+
+        double mediaPonderada = somaNotas / somaPesos;
+
+        System.out.println("A média ponderada é: " + mediaPonderada);
+    }
+}
